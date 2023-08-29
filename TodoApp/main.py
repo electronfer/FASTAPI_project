ac172fdf79856dd0438b9ecb3936c02fd76635ca
electronfer <49488561+electronfer@users.noreply.github.com>
2023-08-29 16:02:58 +0000

    Creating post request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException, Path
 import TodoApp.models as models
 from TodoApp.models import Todos
+from pydantic import BaseModel, Field
 from TodoApp.database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from starlette import status
@@ -19,6 +20,21 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(ge=1, le=5)
+    complete: bool
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'A new TODO',
+                'description': 'A new description of TODO',
+                'priority': 5,
+                'complete': False
+            }
+        }
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
@@ -31,3 +47,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(ge=1)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='Todo not found.')
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
