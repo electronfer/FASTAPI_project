bf9c8b3749f860390e3e3614cc28475ffcabfd36
electronfer <49488561+electronfer@users.noreply.github.com>
2023-08-29 20:54:34 +0000

    Saving users in database

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,12 +1,24 @@
-from fastapi import APIRouter
-from TodoApp.models import Users
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from starlette import status
+from TodoApp.models import Users
+from sqlalchemy.orm import Session
+from TodoApp.database import SessionLocal
 from passlib.context import CryptContext
+from starlette import status
 
 router = APIRouter()
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -29,8 +41,8 @@ class CreateUserRequest(BaseModel):
         }
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_user(create_user_request: CreateUserRequest):
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     
     create_user_model = Users(
         email=create_user_request.email,
@@ -41,7 +53,6 @@ async def create_user(create_user_request: CreateUserRequest):
         hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-    
-    return create_user_model
-    #db.add(create_user_model)
-    #db.commit()
+
+    db.add(create_user_model)
+    db.commit()
