e03a455b9966ff221b57c82a7ea6edd549104646
electronfer <49488561+electronfer@users.noreply.github.com>
2023-08-30 23:51:43 +0000

    Creating a new router admin

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import TodoApp.models as models
 from TodoApp.database import engine
-from TodoApp.routers import auth, todos
+from TodoApp.routers import admin, auth, todos
 
 app = FastAPI()
 
@@ -9,3 +9,4 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
